e9ff679632e00b44b29ed132ed9dd21d0503c9c1
manjushsh <manjushsh@protonmail.com>
2022-01-09 19:09:08 +0530

    Language support added

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import CONFIG_SERVICE from './services/config-service';
 import DataGenerationService from './services/data-generation-servive';
 import FAKER_SERVICE from './services/faker-service';
+import Locale from './locale-options.json';
 
 const app = express();
 const { PORT } = CONFIG_SERVICE;
@@ -15,14 +16,41 @@ app.get('/', (req, res) => {
 
 app.get('/available-types', (req, res) => res.send(DATA_KEYS));
 
-app.get('/custom/:type', (req, res) => {
+// Language with all data - SET B
+app.get('/data/:type', (req, res) => {
     const parameter = req?.params?.type;
     if (DATA_KEYS.includes(parameter)) {
+        const data = DataGenerationService.fakerKeyToValueMapper("en", parameter);
+        res.send({ data });
+    }
+    else if (Locale.locale.includes(parameter)) {
         const data = DataGenerationService.fakerKeyToValueMapper(parameter);
         res.send({ data });
     }
-    else{
-        res.send({error: "No data can be generated for given type!"})
+    else {
+        const data = DataGenerationService.fakerKeyToValueMapper();
+        res.send({ data });
+    }
+});
+
+// Language and Type - SETA+SET B || TYPE Data SET A
+app.get('/data/:type/:locale', (req, res) => {
+    const type = req?.params?.type;
+    const locale = req?.params?.locale;
+    if (DATA_KEYS.includes(type) && Locale.locale.includes(locale)) {
+        const data = DataGenerationService.fakerKeyToValueMapper(locale, type);
+        res.send({ data });
+    }
+    else if (DATA_KEYS.includes(type)) {
+        const data = DataGenerationService.fakerKeyToValueMapper("en", type);
+        res.send({ data });
+    }
+    else if (Locale.locale.includes(locale)) {
+        const data = DataGenerationService.fakerKeyToValueMapper(locale);
+        res.send({ data });
+    }
+    else {
+        res.send({ error: "No data can be generated for given type!" })
     }
 });
 
--- services/data-generation-servive.js
+++ services/data-generation-servive.js
@@ -1,9 +1,9 @@
 import FAKER_SERVICE from "./faker-service";
 
 const DataGenerationService = {
-    fakerKeyToValueMapper: (key) => {
+    fakerKeyToValueMapper: (locale = "en", key) => {
         const maps = {};
-        Object.keys(FAKER_SERVICE.dataGenerationFunctions).forEach((keyPair) => maps[keyPair] = FAKER_SERVICE.dataGenerationFunctions[keyPair]());
+        Object.keys(FAKER_SERVICE.dataGenerationFunctions).forEach((keyPair) => maps[keyPair] = FAKER_SERVICE.dataGenerationFunctions[keyPair](locale));
         return key ? maps[key] : maps;
     },
 };
